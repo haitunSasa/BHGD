55fae78bc97203441f0e1dfdb9b78fd46c948d1c
liyan <947299531@qq.com>
2017-06-17 17:02:34 +0800

    关注

--- src/main/java/com/Service/Impl/FollowServiceImpl.java
+++ src/main/java/com/Service/Impl/FollowServiceImpl.java
@@ -1,13 +1,32 @@
 package com.Service.Impl;
 
+import com.Dao.BaseDaoI;
 import com.Entity.Follow;
 import com.Service.FollowService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by liyan on 2017/4/16.
  */
 @Service
 public class FollowServiceImpl extends BaseServiceImpl<Follow> implements FollowService {
+    @Autowired
+    private BaseDaoI<Follow> dao;
+    private List<Follow> followList = new ArrayList<>();
 
+    @Override
+    public List<Follow> findByUIdAndFId(int userId, int followId) {
+        Map<String,Object> params = new HashMap<>();
+        params.put("userId",userId);
+        params.put("followId",followId);
+        String sql="select t from Follow t where t.fromUserId:=userId and t.toUserId:=followId)";
+        followList = this.dao.find(sql,params);
+        return followList;
+    }
 }
